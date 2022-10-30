e8e25742dd1cad8eb9891a0c66a5e1835ae05629
naeunkim <naeunkim.business@gmail.com>
2022-10-31 01:41:26 +0900

    sentences function

--- aifunction.py
+++ aifunction.py
@@ -1,29 +1,28 @@
 import openai
-import googletrans
-import json
 
 openai.api_key = "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"
 
 def hexcode(input):
-    # translate input
-    translator = googletrans.Translator()
-    result = translator.translate(input, dest="en")
-    # use openai
     response = openai.Completion.create(
-    model="text-davinci-002",
-    prompt="The CSS code for a color like " + result.text + "\nbackground-color: #",
-    temperature=0,
-    max_tokens=64,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=[";"]
+        model="text-davinci-002",
+        prompt="The CSS code for a color like :" + input + "\n\nbackground-color: #",
+        temperature=0,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=[";"]
     )
     return response.choices[0].text
 
-"""
-def translate(input):
-    translator = googletrans.Translator()
-    result = translator.translate(input, dest="en")
-    return result.text
-"""
+def sentences(input):
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=input + "\n\nYou can be decribed in six sentences below:",
+        temperature=0,
+        max_tokens=64,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    return response.text
